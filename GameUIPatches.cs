a753c3dec57d0cc7f851f61c07845455e527dd16
Eclassic32 <eclassic32004@gmail.com>
2026-09-04 21:47:07 +0500

    Repurpose back button in Creative Mode Screen to Start Screen

--- GameUIPatches.cs
+++ GameUIPatches.cs
@@ -11,6 +11,7 @@ namespace PolytopiaArchipelagoMW;
 public static class GameUIPatches
 {
     private static ManualLogSource logger = new("apmw: GameUIPatches");
+    private static UIManager? uiManager;
 
     public static void Load(ManualLogSource logger)
     {
@@ -20,17 +21,16 @@ public static class GameUIPatches
         logger.LogInfo("GameUIPatches loaded");
     }
 
-    // Jump from New Game to Creative Mode Screen
-    // [HarmonyPostfix]
-    // [HarmonyPatch(typeof(StartScreen_UI2), nameof(StartScreen_UI2.ClickNewGame))]
-    // private static bool StartScreen_UI2_JumpToCreative_Postfix(StartScreen_UI2 __instance)
-    // {
-    //     if (!Archipelago.isConnected) { return true; }
-    //     logger.LogInfo("GameModeScreen_UI2.OnShow called. Jumping to Creative Mode screen.");
-    //     __instance.
-    //     return false;
-    // }
+    [HarmonyPostfix]
+    [HarmonyPatch(typeof(StartScreen_UI2), nameof(StartScreen_UI2.OnShow))]
+    private static void StartScreen_UI2_OnShow_Postfix(StartScreen_UI2 __instance)
+    {
+        if (uiManager != null) { return; }
+        uiManager = UnityEngine.Object.FindObjectOfType<UIManager>();
+        logger.LogInfo("UIManager found: " + (uiManager != null ? "Yes" : "No"));
+    }
 
+    // Jump from New Game to Creative Mode Screen
     [HarmonyPrefix]
     [HarmonyPatch(typeof(GameModeScreen_UI2), nameof(GameModeScreen_UI2.OnShow))]
     private static bool GameModeScreen_UI2_JumpToCreative_Prefix(GameModeScreen_UI2 __instance)
@@ -46,31 +46,33 @@ public static class GameUIPatches
         return false;
     }
 
+    // Repurpose back button in Creative Mode Screen to Start Screen
+    [HarmonyPostfix]
+    [HarmonyPatch(typeof(TribePickerScreen_UI2), nameof(TribePickerScreen_UI2.Init))]
+    private static void TribePickerScreen_UI2_Init_Postfix(TribePickerScreen_UI2 __instance, RectTransform transform)
+    {
+        logger.LogInfo($"TribePickerScreen_UI2.Init called");
+        var backButton = __instance.backButton;
+        backButton.ClearCallbacks();
+        backButton.OnClickedSignal.Add(DelegateSupport.ConvertDelegate<Il2CppSystem.Action>(JumpToMain));
+    }
+
+    internal static void JumpToMain()
+    {
+        var mainScreen = UIConstants.Screens.StartScreen;
+        uiManager?.ShowScreen(mainScreen);
+        
+    }
 
     // --- EXPLORATION ---
 
     // In game > Game Stats
-    [HarmonyPrefix]
-    [HarmonyPatch(typeof(GameModeUtils), nameof(GameModeUtils.GetTitle))]
-    private static void GameModeUtils_GetTitle_Prefix(GameMode gameMode, ref string __result)
-    {
-        logger.LogInfo($"GameModeUtils.GetTitle called for game mode: {gameMode}. Result: {__result}");
-    }
-
-
-    // private static UIRoundButton_UI2? backButton = null;
-
-    // [HarmonyPostfix]
-    // [HarmonyPatch(typeof(TribePickerScreen_UI2), nameof(TribePickerScreen_UI2.Init))]
-    // private static void TribePickerScreen_UI2_Init_Postfix(TribePickerScreen_UI2 __instance, RectTransform transform)
+    // [HarmonyPrefix]
+    // [HarmonyPatch(typeof(GameModeUtils), nameof(GameModeUtils.GetTitle))]
+    // private static void GameModeUtils_GetTitle_Prefix(GameMode gameMode, ref string __result)
     // {
-    //     logger.LogInfo($"TribePickerScreen_UI2.Init called");
-    //     var backButton = __instance.backButton;
-    //     backButton.OnClickedSignal.Add(DelegateSupport.ConvertDelegate<Il2CppSystem.Action>(JumpToMain));
+    //     logger.LogInfo($"GameModeUtils.GetTitle called for game mode: {gameMode}. Result: {__result}");
     // }
 
-    // internal static void JumpToMain(TribePickerScreen_UI2 __instance)
-    // {
-    //     __instance.
-    // }
+    
 }
